368ffb6b6f2f56e5bd3fb7438fa8d85f50ddf87b
Kim Mora <kmorat27@outlook.com>
2025-04-21 13:01:21 -0600

    add model scripts

---
+++ src/feature_generation.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from pandas import DataFrame
+
+class OutageFeatures:
+    """
+     Creates all features for outage Model.
+    """
+    def __init__(self, data:DataFrame) -> None:
+        self.df = data
+        self.no_work_cols = ['time', 
+                             'episode_fips_id', 
+                             'meteorological_current_datetime_val', 
+                             'outage_in_an_hour', 
+                             'hours_to_outage']
+        self.work_cols = [col for col in self.df.columns
+                          if col not in self.no_work_cols]
+
+    def get_diff_features(self)->None:
+        """Generates features about:
+            - Absolute diference between temperature at 10 meters 
+                and temperature at 2 meteres.
+            - Absolute diference between wind speed at 50 meters
+                and wind speed at 2 meters. 
+        """
+        self.df['abs_diff_between_t2m_t10m'] = self.df.apply(lambda row: abs(row['T2M'] \
+                                                                        - row['T10M']), axis=1)
+        self.df['abs_diff_between_ws50m_wd2m'] = self.df.apply(lambda row: abs(row['WS50M'] \
+                                                                        - row['WS2M']), axis=1)
+
+    def get_feature_previous_n_hours(self, col:str, n:int)-> None:
+        """For a given column, it generates a new column
+           with the values for the previous n hours. 
+
+        Args:
+            col (str): column to be calculate
+            n (int): hours back to get the value 
+        """
+        self.df[f'{col}_{n}_hours_ago'] = self.df.groupby('episode_fips_id')[col].shift(n)
+
+    def get_delta_featues(self, col:str)-> None:
+        """For a given column it calculate:
+            - Diference between the current value vs the value 
+            n hours before (in this case 1, 2 and 3 hous)
+            - Diference between value hours (i.e. the value 1 hour befor vs the value 3 hous before)
+
+        Args:
+            col (str): column to calculate the deltas.
+        """
+        self.df[f'{col}_delta_one_hour'] = self.df.apply(lambda row: row[col] \
+                                                        - row[f'{col}_an_hour_ago'], axis=1)
+        self.df[f'{col}_delta_two_hour'] = self.df.apply(lambda row: row[col] \
+                                                        - row[f'{col}_two_hours_ago'], axis=1)
+        self.df[f'{col}_delta_three_hour'] = self.df.apply(lambda row: row[col] \
+                                                     - row[f'{col}_three_hours_ago'], axis=1)
+        self.df[f'{col}_delta_previous'] = self.df.apply(lambda row: row[f'{col}_an_hour_ago'] \
+                                                        - row[f'{col}_two_hours_ago'], axis=1)
+        self.df[f'{col}_delta_two_previous'] = self.df.apply(lambda row:
+                                                                row[f'{col}_two_hours_ago'] \
+                                                        - row[f'{col}_three_hours_ago'], axis=1)
+
+    def get_tendency_features(self, col:str) -> None:
+        """Calculate the tendency of the values with the follow logic:
+           - if the value increase the tendency will be equal to 1 
+           - if the value mantains the tendency will be equal to 0
+           - if the value decrease the tendency will be equal to 0 
+
+        Args:
+            col (str): Column to calculate features.
+        """
+        self.df[f'{col}_previous_tendency'] = self.df[f'{col}_delta_previous'].apply(
+                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
+        self.df[f'{col}_two_previous_tendency'] = self.df[f'{col}_delta_two_previous'].apply(
+                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
+        self.df[f'{col}_current_tendency'] = self.df[f'{col}_delta_one_hour'].apply(
+                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
+
+    def get_features(self)->DataFrame:
+        """Create all features for the outage model. 
+
+        Returns:
+            DataFrame: dataframe with all the features calculated. 
+        """
+        self.df.sort_values(by=['episode_fips_id',
+                                'meteorological_current_datetime_val'], 
+                                inplace=True)
+        self.get_diff_features()
+        for col in self.work_cols:
+            for ix in range(1, 3):
+                self.get_feature_previous_n_hours(col, ix)
+            self.get_delta_featues(col)
+            self.get_tendency_features(col)
+
+        return self.df
---
+++ src/model_evaluation.py
@@ -0,0 +1,71 @@
+from sklearn.metrics import roc_auc_score,\
+                precision_recall_curve, auc
+import pandas as pd
+import shap
+
+class ModelEvaluation:
+    def __init__(self, model, x_test, y_test,
+                        x_train, y_train) -> None:
+        self.model = model
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.explainer = None
+        self.shap_values = None
+        self.pred_test = None
+        
+    def get_model_auc_metric(self):
+        """
+        Get the area under the ROC curve for train and test set. 
+        Args:
+            model (any): model to evaluate
+            x_test (DataFrame): test set features
+            y_test (Series): test labels 
+            x_train (DataFrame): train set features
+            y_train (Series): train labels
+        """
+        self.pred_test = pd.Series(self.model.predict_proba(self.x_test)[:, 1])
+        self.pred_test.index = self.x_test.index
+        test_auc = roc_auc_score(self.y_test, self.pred_test)
+        pred_train = pd.Series(self.model.predict_proba(self.x_train)[:, 1])
+        pred_train.index = self.x_train.index
+        train_auc = roc_auc_score(self.y_train, pred_train)
+        print(f"Train set AUC:{train_auc} \n")
+        print(f"Test set AUC: {test_auc}")
+
+    def get_model_aucp(self):
+        """
+        Get the area under the Precision-Recall curve.
+
+        Args:
+            model (any): model to evaluate. 
+            x_test (DataFrame): test set features
+            y_test (Series): test labels.
+        """
+        precision, recall, _ = precision_recall_curve(self.y_test,
+                                                      self.pred_test)
+        aucpr = auc(recall, precision)
+        print(f"Test set AUCPR: {aucpr}")
+
+    def get_shap_values(self):
+        self.explainer = shap.TreeExplainer(self.model,
+                                            self.x_test)
+        self.shap_values = self.explainer.shap_values(
+                                            self.x_test
+                                        )
+
+    def get_shap_summary_plot(self):
+        shap.summary_plot(self.shap_values,
+                          self.x_test)
+
+    def get_shap_decision_plot(self, response):
+        index = 257182
+        outage_id = "99962_34029_2015100113"
+        if response == 0:
+            index = 3
+            outage_id = "100009_55009_2015081417"
+        shap.decision_plot(self.explainer.expected_value,
+                            self.shap_values[index,:],
+                            self.x_test.loc[outage_id,:],
+                            link='logit')
