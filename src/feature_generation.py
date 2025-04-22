ace338dd6bb7e63b637492f5c2c7867e605136a8
dhdzmota <dhdzmota@gmail.com>
2025-04-21 23:29:55 -0500

    Improved format, according to flake8 and style. Changed some feature logic to enhance feature computation (Replacing apply with direct changes on the DataFrame.)

--- src/feature_generation.py
+++ src/feature_generation.py
@@ -1,91 +1,85 @@
-import pandas as pd
 from pandas import DataFrame
+
+RANGE = [1, 2, 3]
+
 
 class OutageFeatures:
     """
      Creates all features for outage Model.
     """
-    def __init__(self, data:DataFrame) -> None:
+    def __init__(self, data: DataFrame) -> None:
         self.df = data
-        self.no_work_cols = ['time', 
-                             'episode_fips_id', 
-                             'meteorological_current_datetime_val', 
-                             'outage_in_an_hour', 
-                             'hours_to_outage']
-        self.work_cols = [col for col in self.df.columns
-                          if col not in self.no_work_cols]
+        self.no_work_cols = [
+            'time',
+            'episode_fips_id',
+            'meteorological_current_datetime_val',
+            'outage_in_an_hour',
+            'hours_to_outage'
+        ]
+        self.work_cols = [
+            col for col in self.df.columns if col not in self.no_work_cols
+        ]
 
-    def get_diff_features(self)->None:
+    def get_diff_features(self) -> None:
         """Generates features about:
-            - Absolute diference between temperature at 10 meters 
-                and temperature at 2 meteres.
-            - Absolute diference between wind speed at 50 meters
-                and wind speed at 2 meters. 
+        - Diference between temperature at 10 meters and at 2 meters.
+        - Diference between Wind speed at 50 meters and at 2 meters.
+
+        :return:
         """
-        self.df['abs_diff_between_t2m_t10m'] = self.df.apply(lambda row: abs(row['T2M'] \
-                                                                        - row['T10M']), axis=1)
-        self.df['abs_diff_between_ws50m_wd2m'] = self.df.apply(lambda row: abs(row['WS50M'] \
-                                                                        - row['WS2M']), axis=1)
+        self.df['diff_between_t10m_t2m'] = self.df['T10M'] - self.df['T2M']
+        self.df['diff_between_t50m_t2m'] = self.df['WS50M'] - self.df['WS2M']
 
-    def get_feature_previous_n_hours(self, col:str, n:int)-> None:
-        """For a given column, it generates a new column
-           with the values for the previous n hours. 
+    def get_feature_previous_n_hours(self, col: str, n: int) -> None:
+        """For a given column, it generates a new column with the values for the previous n hours.
 
-        Args:
-            col (str): column to be calculate
-            n (int): hours back to get the value 
+        :param col: str Column to be calculated
+        :param n: Hours back to get the value
+        :return:
         """
         self.df[f'{col}_{n}_hours_ago'] = self.df.groupby('episode_fips_id')[col].shift(n)
 
-    def get_delta_featues(self, col:str)-> None:
-        """For a given column it calculate:
-            - Diference between the current value vs the value 
-            n hours before (in this case 1, 2 and 3 hours)
-            - Diference between value hours (i.e. the value 1 hour before vs the value 3 hours before)
+    def get_delta_featues(self, col: str) -> None:
+        """For a given column it calculates:
+        - Diference between the current value vs the value n hours before (in this case 1, 2 and 3 hours)
+        - Diference between value hours (i.e. the value 1 hour before vs the value 3 hours before)
 
-        Args:
-            col (str): column to calculate the deltas.
+        :param col: str Column to calculate the deltas
+        :return:
         """
-        self.df[f'{col}_delta_one_hour'] = self.df.apply(lambda row: row[col] \
-                                                        - row[f'{col}_an_hour_ago'], axis=1)
-        self.df[f'{col}_delta_two_hour'] = self.df.apply(lambda row: row[col] \
-                                                        - row[f'{col}_two_hours_ago'], axis=1)
-        self.df[f'{col}_delta_three_hour'] = self.df.apply(lambda row: row[col] \
-                                                     - row[f'{col}_three_hours_ago'], axis=1)
-        self.df[f'{col}_delta_previous'] = self.df.apply(lambda row: row[f'{col}_an_hour_ago'] \
-                                                        - row[f'{col}_two_hours_ago'], axis=1)
-        self.df[f'{col}_delta_two_previous'] = self.df.apply(lambda row:
-                                                                row[f'{col}_two_hours_ago'] \
-                                                        - row[f'{col}_three_hours_ago'], axis=1)
+        self.df[f'{col}_delta_one_hour'] = self.df[col] - self.df[f'{col}_an_hour_ago']
+        self.df[f'{col}_delta_two_hour'] = self.df[col] - self.df[f'{col}_two_hours_ago']
+        self.df[f'{col}_delta_three_hour'] = self.df[col] - self.df[f'{col}_three_hours_ago']
+        self.df[f'{col}_delta_previous'] = self.df[f'{col}_an_hour_ago'] - self.df[f'{col}_two_hours_ago']
+        self.df[f'{col}_delta_two_previous'] = self.df[f'{col}_two_hours_ago'] - self.df[f'{col}_three_hours_ago']
 
-    def get_tendency_features(self, col:str) -> None:
+    def get_tendency_features(self, col: str) -> None:
         """Calculate the tendency of the values with the follow logic:
-           - if the value increase the tendency will be equal to 1 
-           - if the value mantains the tendency will be equal to 0
-           - if the value decrease the tendency will be equal to 0 
-
-        Args:
-            col (str): Column to calculate features.
+        - if the value increase the tendency will be equal to 1
+        - if the value mantains the tendency will be equal to 0
+        - if the value decrease the tendency will be equal to 0
+        :param col: str Column to calculate features.
+        :return:
         """
-        self.df[f'{col}_previous_tendency'] = self.df[f'{col}_delta_previous'].apply(
-                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
-        self.df[f'{col}_two_previous_tendency'] = self.df[f'{col}_delta_two_previous'].apply(
-                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
-        self.df[f'{col}_current_tendency'] = self.df[f'{col}_delta_one_hour'].apply(
-                                                    lambda x: 1 if x> 0 else  (-1 if x <0 else 0))
+        def tendency_func(x):
+            return 1 if x > 0 else (-1 if x < 0 else 0)
 
-    def get_features(self)->DataFrame:
-        """Create all features for the outage model. 
+        self.df[f'{col}_previous_tendency'] = self.df[f'{col}_delta_previous'].apply(tendency_func)
+        self.df[f'{col}_two_previous_tendency'] = self.df[f'{col}_delta_two_previous'].apply(tendency_func)
+        self.df[f'{col}_current_tendency'] = self.df[f'{col}_delta_one_hour'].apply(tendency_func)
 
-        Returns:
-            DataFrame: dataframe with all the features calculated. 
+    def get_features(self) -> DataFrame:
+        """Create all features for the outage model.
+
+        :return: DataFrame: dataframe with all the features calculated.
         """
-        self.df.sort_values(by=['episode_fips_id',
-                                'meteorological_current_datetime_val'], 
-                                inplace=True)
+        self.df.sort_values(
+            by=['episode_fips_id', 'meteorological_current_datetime_val'],
+            inplace=True
+        )
         self.get_diff_features()
         for col in self.work_cols:
-            for ix in range(1, 3):
+            for ix in RANGE:
                 self.get_feature_previous_n_hours(col, ix)
             self.get_delta_featues(col)
             self.get_tendency_features(col)
