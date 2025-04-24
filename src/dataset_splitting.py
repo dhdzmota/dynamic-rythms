a673ec6fbf7af0b4147334bb6eb2b08aa2a425e8
dhdzmota <dhdzmota@gmail.com>
2025-04-24 16:32:59 -0500

    Changes in dataset_splitting.py format, included the RANDOMSEED change.

--- src/dataset_splitting.py
+++ src/dataset_splitting.py
@@ -6,20 +6,21 @@ import src.feature_generation as feature_generation
 import src.utils as utils
 
 
-config = utils.get_config_file()
-RANDOM_SEED = config['RANDOM_SEED']
+RANDOM_SEED = utils.RANDOM_SEED
 TRAIN_WINDOW_DATA_PERCENTAGE = 0.8
 OOT_WINDOW_DATA_PERCENTAGE = 0.15
 
-SPLITTED_FILENAMES = ['train', 'test', 'OOT', 'eval', 'cal']
+SPLITTED_FILENAMES = ['train', 'test', 'eval', 'cal', 'OOT']
 
 INTERIM_DATA_PATH = utils.get_data_path('interim')
 
 # Splits train 80 and test 20
 TEST_SIZE = 0.2
-# From the remaining train, splits into a transitory set, which will be converted into cal and eval sets
+# From the remaining train, splits into a transitory set,
+# which will be converted into cal and eval sets
 TRANS_SIZE = 0.2
-# Split of the eval and cal to be CAL_EVAL_SIZE  and 1-CAL_EVAL_SIZE, corresponding to TRANSIZE/2 form the remining train
+# Split of the eval and cal to be CAL_EVAL_SIZE  and 1-CAL_EVAL_SIZE,
+# corresponding to TRANSIZE/2 form the remining train
 CAL_EVAL_SIZE = 0.5
 
 
@@ -40,19 +41,32 @@ def get_train_and_oot_candidates():
         normalize=True
     ).sort_index().cumsum()
     # Get dates to split temporal data at a desired corresponding percentage.
-    print(f'Train window data has {TRAIN_WINDOW_DATA_PERCENTAGE*100}% of data.')
-    print(f'OOT window data has {OOT_WINDOW_DATA_PERCENTAGE*100}% of data.')
-    print(f'We are discarding {(1-TRAIN_WINDOW_DATA_PERCENTAGE-OOT_WINDOW_DATA_PERCENTAGE)*100}% of data.')
-    max_train_date = episode_fips_dates_max[episode_fips_dates_max < TRAIN_WINDOW_DATA_PERCENTAGE].tail(1).index[0]
-    min_oot_date = episode_fips_dates_min[episode_fips_dates_min > (1-OOT_WINDOW_DATA_PERCENTAGE)].head(1).index[0]
+    train_perc = TRAIN_WINDOW_DATA_PERCENTAGE*100
+    oot_perc = OOT_WINDOW_DATA_PERCENTAGE*100
+    print(f'Train window data has {train_perc}% of data.')
+    print(f'OOT window data has {oot_perc}% of data.')
+    dis_data = (1-TRAIN_WINDOW_DATA_PERCENTAGE-OOT_WINDOW_DATA_PERCENTAGE)*100
+    print(f'We are discarding {dis_data}% of data.')
+    max_train_date = episode_fips_dates_max[
+        episode_fips_dates_max < TRAIN_WINDOW_DATA_PERCENTAGE
+    ].tail(1).index[0]
+    min_oot_date = episode_fips_dates_min[
+        episode_fips_dates_min > (1-OOT_WINDOW_DATA_PERCENTAGE)
+    ].head(1).index[0]
     # Split the dataframe, get the corresponding episode_fips ids.
-    train_window_candidates = episode_fips_dates[episode_fips_dates.max_date < max_train_date].index
-    oot_window_candidates = episode_fips_dates[episode_fips_dates.max_date > min_oot_date].index
+    train_window_candidates = episode_fips_dates[
+        episode_fips_dates.max_date < max_train_date
+    ].index
+    oot_window_candidates = episode_fips_dates[
+        episode_fips_dates.max_date > min_oot_date
+    ].index
     print('Done.')
     return data, train_window_candidates, oot_window_candidates
 
 
-def train_test_cal_eval_splits(train_window_candidates, oot_window_candidates, print_percentages=True):
+def train_test_cal_eval_splits(
+        train_window_candidates, oot_window_candidates, print_percentages=True
+):
     print('Splitting datasets')
     print('Getting test dataset...')
     train_episode_fips_id, test_episode_fips_id = train_test_split(
@@ -64,7 +78,9 @@ def train_test_cal_eval_splits(train_window_candidates, oot_window_candidates, p
     )
     print('Getting cal and eval datasets...')
     cal_episode_fips_id, eval_episode_fips_id = train_test_split(
-        transitory_episode_fips_id, test_size=CAL_EVAL_SIZE, random_state=RANDOM_SEED
+        transitory_episode_fips_id,
+        test_size=CAL_EVAL_SIZE,
+        random_state=RANDOM_SEED
     )
     print('Done.')
     datasets_fips_ids = {
@@ -127,7 +143,9 @@ def save_datasets(datasets):
 
 
 def splitting_process(save=True, return_data=False):
-    data, train_window_candidates, oot_window_candidates = get_train_and_oot_candidates()
+    data, train_window_candidates, oot_window_candidates = (
+        get_train_and_oot_candidates()
+    )
     datasets_fips_ids = train_test_cal_eval_splits(
         train_window_candidates, oot_window_candidates
     )
